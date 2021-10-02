80272ed299ec22c801b3ea52c770b8c405c91b25
Pedro Batista de Almeida Filho <pedro.filho@hbsis.com.br>
2021-10-02 19:44:23 -0300

    preparing for stable version

--- first_app.py
+++ first_app.py
@@ -4,8 +4,14 @@
 # https://www.opencyclemap.org/
 # https://www.cyclosm.org/#map=12/-12.9654/-38.3862/cyclosm
 # https://www.bicyclenetwork.com.au/tips-resources/maps-and-rides/bike-trail-maps/
-# FAZER CONSULTAS NO OPEN STREET MAP
-#  CRIAR DATAFRAME E PLOTAR NO NOSSO MAPA
+# https://stackoverflow.com/questions/31684375/automatically-create-requirements-txt
+    # pip freeze?
+
+# https://cNLw104.na1.hubspotlinks.com/Btc/ZT+113/cNLw104/VVWKXh2mdDRgW8hkpwZ1tlGkgVcSh174xcQpJMZjDWN3lScmV1-WJV7CgMNXW2zw67T83qr7nW3CVgRF2RR28RW79WDg68F5RvDW1nCXtN2cD6FrW3z6v7l49jpnRW4hs3Gh3DVMHJW24tXlS1YL8q5VLrKmf63ZPz0W7-YK5z4Cwt96W2CmFk76B5x5hW55p6Kl4Gh-rwW2RDj3T2CwXm4VNKS6f1BZ8LDW26cYBc444gcGW6h-qqL7zTZs0W3yznkC2N0D8GVXk7-Q8DhJ2sW3-Y96B5CwPXgN540zl29rm_GW393kqt8KQ4PNW3JM6LM1lJVY9W23FwgN4tnH6vW8Gnczh1YfzzvW8pllsj4MGWkZW3wPP4G4T-QjrW6GdMgN4yB7zS36CW1
+
+# OBJETIVO:
+    # FAZER CONSULTAS NO OPEN STREET MAP
+    # CRIAR DATAFRAME E PLOTAR NO NOSSO MAPA
 
 import numpy as np
 import pandas as pd
